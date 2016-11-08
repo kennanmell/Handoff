06efa09844a65e6152f034d46d5bc1774964097c
Kennan Mell <kmell96@cs.washington.edu>
2016-11-07 21:59:22 -0800

    fbsdk and nav

--- android/app/src/main/java/com/handoff/MainActivity.java
+++ android/app/src/main/java/com/handoff/MainActivity.java
@@ -1,6 +1,8 @@
 package com.handoff;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -10,6 +12,13 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "HandOff";
+        return "MainNavigator";
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
 }
