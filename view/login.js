e5bef24858e601909c326273fe7215c4caaa7d51
Kennan Mell <kmell96@cs.washington.edu>
2016-11-11 15:47:39 -0800

    fix bug

--- view/login.js
+++ view/login.js
@@ -21,8 +21,17 @@ var Login = React.createClass({
         <LoginButton
           publishPermissions={["public_profile"]}
           onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                alert("Login was successful with permissions: " + result.grantedPermissions)
+              }
+            }
           }
-          onLogoutFinished={}/>
+          onLogoutFinished={() => alert("User logged out")}/>
       </View>
     );
   }
