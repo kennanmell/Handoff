ca24e19703307dff0a30149b7c3d8d3582a3b3d5
Peter Giseburt <petergg@uw.edu>
2016-11-11 17:45:11 -0800

    comments

--- view/RequestFeed.js
+++ view/RequestFeed.js
@@ -2,20 +2,11 @@
 requests will display a pop up of the requests. The user will also be able to click on the
 organization to go to the page of the organization. */
 
-/*
-        Things I need to do:
-        1. Get a function call to get real data, which needs to be tranformed into requests
-        2. Turn those requests into requestcomponents and put them in the bottom thing
-        3. Figure out how to send variables through a callback
-        4. make a compentent that i can both initialize and pass in props so that we dont have
-        to hardcode the data for requests
-        */
 import React, { Component, PropTypes } from 'react';
 import { TouchableHighlight, TextInput,
             StyleSheet, AppRegistry, ListView, Text, View } from 'react-native';
-//import Request from './Request';
 
-// These are different styles
+// These are different styles that components use.
 const styles = StyleSheet.create({
   separator: {
     flex: 1,
@@ -51,7 +42,8 @@ const Header = (props) => (
   </View>
 );
 
-// Returns a promise
+// This method returns a Promise that will at some point, probably very quickly return
+// a list of 30 requests.
 async function getRequests() {
     return fetch('https://u116vqy0l2.execute-api.us-west-2.amazonaws.com/prod/requests', {
       method: 'POST',
@@ -65,6 +57,8 @@ async function getRequests() {
     })
 }
 
+// The row is a component that should have a title, organization, and description passed in as props
+// This component will display these components.
 const Row = (props) => (
   <View style={{padding: 10}}>
     <Text style={{fontSize: 20}}>  {props.title}</Text>
@@ -74,11 +68,14 @@ const Row = (props) => (
   </View>
 );
 
+// This component is a scrollable list of requests. Intially it will display a text that shows we
+// are still waiting on the server to provide our requests, but once the request to the server has
+// been completed the received requests will be displayed in the scrollable list.
 export default class RequestFeed extends Component {
     constructor(props) {
         super(props);
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        requests = [{
+        startPage = [{
            "organization": "HandOff Team",
            "time": 7,
            "title": "Waiting on data",
@@ -86,24 +83,34 @@ export default class RequestFeed extends Component {
                "your internet connection.",
         }];
 
-        getRequests()
-            .then((response) => response.json())
-            .then((responseJson) => {
-                //console.log('title below');
-                //console.log(responseJson.requests[0].title);
-                //result = [responseJson.requests[0], responseJson.requests[1], responseJson.requests[3]];
+        // Sets the initial page to a loading page
+        this.state = {
+          dataSource: ds.cloneWithRows(startPage)
+        }
 
-                this.setState({
-                  dataSource: ds.cloneWithRows(responseJson.requests)
-                });
-              })
+        // fetch returns a promise, and the .then statements will then handle the response
+        // and update the listviews datasource once there is data to update it with.
+        //getRequests()
+            response = fetch('https://u116vqy0l2.execute-api.us-west-2.amazonaws.com/prod/requests', {
+                  method: 'POST',
+                  headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                      limit: 30,
+                  })
+            })
+              .then((response) => response.json())
+              .then((responseJson) => {
+                  this.setState({
+                    dataSource: ds.cloneWithRows(responseJson.requests)
+                  });
+                })
               .catch((error) => {
                 console.error(error);
         });
 
-       this.state = {
-            dataSource: ds.cloneWithRows(requests)
-        }
 
     }
 
