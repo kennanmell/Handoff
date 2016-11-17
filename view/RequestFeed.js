5af5423090f8941e827548819792b3bc3642ea38
Peter Giseburt <petergg@uw.edu>
2016-11-16 21:43:47 -0800

    edit requests kinda does stuff

--- view/RequestFeed.js
+++ view/RequestFeed.js
@@ -3,7 +3,7 @@ requests will display a pop up of the requests. The user will also be able to cl
 organization to go to the page of the organization. */
 
 import React, { Component, PropTypes } from 'react';
-import { TouchableHighlight, TextInput, Alert,
+import { TouchableHighlight, TextInput, Alert, Modal,
             StyleSheet, AppRegistry, ListView, Text, View } from 'react-native';
 import{Button} from 'native-base';
 
@@ -31,20 +31,25 @@ const styles = StyleSheet.create({
       borderRadius: 2,
   },
     orgButton: {
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'flex-end',
-            marginLeft: 3,
-            marginTop: 3,
-            marginRight: 3,
-            borderWidth: 1,
-            borderRadius: 5,
-            padding: 3,
-            borderColor: 'black',
-            marginBottom: 3,
-            backgroundColor: '#642D64',
-            color: 'white',
-        }
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        marginLeft: 3,
+        marginTop: 3,
+        marginRight: 3,
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 3,
+        borderColor: 'black',
+        marginBottom: 3,
+        backgroundColor: '#642D64',
+        color: 'white',
+    },
+  welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+  },
 });
 
 // This header is a search bar and it actually looks good.
@@ -92,16 +97,87 @@ const Row = (props) => (
   </View>
 );
 
-const OrgRow = (props) => (
+/* This is a display of a specific orgs request, with an edit request button that allows one to
+edit the request specified by the row and post that update to the server */
+/*const OrgRow = (props) => (
    <View style={{padding: 10}}>
      <Text style={{fontSize: 20}}>  {props.title}</Text>
      <Text style={{alignItems: 'center'}, {fontSize: 18}}>  {props.description}</Text>
-     <Text style={styles.orgButton} onPress={()=>Alert.alert('Organization Info', null,
-                                            [{text: 'Subscribe', onPress: ()=>console.log('subscribe, yo!')},
-                                            {text: 'Close', onPress:()=>console.log('done')}])}
-                                            > Edit Request </Text>
+     <Text style={styles.orgButton} onPress=props.onPress> Edit Request </Text>
    </View>
-)
+)*/
+
+class OrgRow extends Component {
+    constructor(props) {
+        super(props);
+        this.title = props.title;
+        this.organization = props.organization;
+        this.description = props.description;
+        this.time = props.time;
+        this.tags = props.tags;
+
+        this.state = {
+          modalVisible: false,
+        }
+    }
+
+    setModalVisible(visible) {
+      this.setState({modalVisible: visible});
+    }
+
+    render() {
+      return(
+        <View style={{padding: 10}}>
+          <Modal
+            animationType={"slide"}
+            transparent={false}
+            visible={this.state.modalVisible}
+            onRequestClose={() => {alert("Modal has been closed.")}}
+            >
+            <View style={{marginTop: 22}}>
+              <Text style={styles.welcome}> Handoff</Text>
+              <TextInput
+                style={{height: 40}}
+                placeholder="Give your request a snappy title"
+                onChangeText={(text) => this.setState({requestName: text})}
+                tag='titleInput'
+              />
+               <TextInput
+                    style={{height: 40}}
+                    placeholder="Provide details on what you are requesting"
+                    onChangeText={(text) => this.setState({requestDescription: text})}
+                    tag='detailsInput'
+               />
+               <TextInput
+                style={{height: 40}}
+                placeholder="Tag your request with keywords so people can find it"
+                tag='keywordsInput'
+                onChangeText={(text) => this.setState({requestTags: text})}
+                />
+              <Button
+                  style={styles.button}
+                onPress={() => {Alert.alert('Update Successful', 'Sent your edited request.');
+                    makeRequest(this.state.requestName, this.state.requestDescription);
+                    console.log("attempting to make request"); }}
+                    tag='submitButton'>Submit Request</Button>
+              <Button
+                  style={styles.button}
+                  onPress={() => {this.setModalVisible(!this.state.modalVisible)}}> Cancel
+              </Button>
+            </View>
+          </Modal>
+          <Text style={{fontSize: 20}}>  {this.title}</Text>
+          <Text style={{alignItems: 'center'}, {fontSize: 18}}>  {this.description}</Text>
+          <Text style={styles.orgButton} onPress={() => {this.setModalVisible(true)}}>
+                Edit Request </Text>
+        </View>);
+    }
+}
+
+/*<Text style={styles.orgButton} onPress= {()=>Alert.alert(props.title, props.description,
+                                            [{text: 'Submit Edit', onPress: ()=>console.log('subscribe, yo!')},
+                                            {text: 'Close', onPress:()=>console.log('done')}])}
+                                            > Edit Request </Text>*/
 
 // This component is a scrollable list of requests. Intially it will display a text that shows we
 // are still waiting on the server to provide our requests, but once the request to the server has
@@ -118,10 +194,7 @@ export default class RequestFeed extends Component {
                "your internet connection.",
         }];
 
-        this.organization = null;
-        if (props.organization != null) {
-            this.organization = props.organization;
-        }
+        this.organization = props.organization;
 
         // Sets the initial page to a loading page
         this.state = {
@@ -159,7 +232,7 @@ export default class RequestFeed extends Component {
             <View>
               <ListView
                 dataSource={this.state.dataSource}
-                renderRow={(rowData) => <OrgRow {...rowData} />}
+                renderRow={(rowData) => <OrgRow {...rowData}/>}
                 renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
                 renderHeader={() => <Header />}
               />
